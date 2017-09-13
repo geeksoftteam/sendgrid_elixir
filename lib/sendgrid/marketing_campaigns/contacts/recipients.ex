28179a4608e328695aab077c26f07f22b1b8e1ef
Bartosz Kalinowski <kelostrada@gmail.com>
2017-09-13 14:46:23 +0200

    handle no changes for recipient

--- lib/sendgrid/marketing_campaigns/contacts/recipients.ex
+++ lib/sendgrid/marketing_campaigns/contacts/recipients.ex
@@ -42,9 +42,17 @@ defmodule SendGrid.Contacts.Recipients do
 
     {:error, errors}
   end
-
   # Handles the result when it's valid.
   defp handle_recipient_result(%{body: %{"persisted_recipients" => [recipient_id]}}) do
     {:ok, recipient_id}
+  end
+
+  # Handles the result when there were no returned recipients (for example if it's an update which didn't change anything)
+  defp handle_recipient_result(%{body: %{"persisted_recipients" => []}}) do
+    {:error, ["No changes applied for recipient"]}
+  end
+
+  defp handle_recipient_result(_) do
+    {:error, ["Unexpected error"]}
   end
 end
